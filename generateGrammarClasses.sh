7942a609ae0ea0ff72ef2ab422adf9ae1cc41376
Jake Wharton <jakewharton@gmail.com>
2010-10-24 16:56:20 -0400

    Update generation path for maven standard layout.

--- generateGrammarClasses.sh
+++ generateGrammarClasses.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar lib/antlr-3.2.jar GuiceyData.g -o src/com/lowereast/guiceymongo/data/generator/parser
+java -jar lib/antlr-3.2.jar GuiceyData.g -o src/main/java/com/lowereast/guiceymongo/data/generator/parser
